cb352a1e8da4eabfabec9e0e2dc191f1bd1c0cf3
Adrian Lopez <adrianlzt@gmail.com>
2021-06-02 09:57:28 +0200

    Add websockets support

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -5,7 +5,7 @@ export TARGET_HOST=${TARGET_HOST:-proxyapp}
 export CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-20M}
 
 # Hack to avoid breaking nginx.conf
-export host='$host' remote_addr='$remote_addr' proxy_add_x_forwarded_for='$proxy_add_x_forwarded_for' scheme='$scheme' remote_addr='$remote_addr'
+export host='$host' remote_addr='$remote_addr' proxy_add_x_forwarded_for='$proxy_add_x_forwarded_for' scheme='$scheme' http_upgrade='$http_upgrade' remote_addr='$remote_addr'
 
 envsubst < /nginx.conf.template > /etc/nginx/nginx.conf
 
